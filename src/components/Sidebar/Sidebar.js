0434d61ae93e4b123da4e305c2dd20484567e5db
Yash Virani <viraniyash2001@gmail.com>
2024-05-23 17:26:27 +0530

    Sidebar dropdown

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import CategoryIcon from '@mui/icons-material/Category';
@@ -8,11 +8,14 @@ import SellIcon from '@mui/icons-material/Sell';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import AddBusinessIcon from '@mui/icons-material/AddBusiness';
 import Avatar from '@mui/joy/Avatar';
-
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import AssessmentIcon from '@mui/icons-material/Assessment';
 
 import './Sidebar.css';
 
 function Sidebar() {
+  let [sales,setSales] = useState(false);
+  let [purchase,setPurchase] = useState(false);
   return (
     <div className='sidebar' >
       <div className='side_avatar'>
@@ -32,11 +35,14 @@ function Sidebar() {
          <CategoryIcon />
          <p style={{marginLeft : '1vw'}}>Items</p>
       </div>
-      <div className='side_function'>
+      <div className='side_function' onClick={() => {
+         setSales(!sales);
+         setPurchase(false);
+      }}>
          <ReceiptIcon />
          <p style={{marginLeft : '1vw'}}>Sale</p>
       </div>
-      <div className='sale_dropdown'>
+      <div className={sales ? 'sale_dropdown' : 'remove_dropdown'} >
          <div className='sale_dropdown_tab'>
            <p>Sale Invoices</p>
          </div>
@@ -57,9 +63,26 @@ function Sidebar() {
          </div>
          
       </div>
-      <div className='side_function'>
+      <div className='side_function' onClick={() => {
+         setPurchase(!purchase)
+         setSales(false);
+      }}>
          <ShoppingCartIcon />
          <p style={{marginLeft : '1vw'}}>Purchase</p>
+      </div>
+      <div className={purchase ? 'sale_dropdown' : 'remove_dropdown'} >
+         <div className='sale_dropdown_tab'>
+           <p>Purchase Bills</p>
+         </div>
+         <div className='sale_dropdown_tab'>
+           <p>Payment Out</p>
+         </div>
+         <div className='sale_dropdown_tab'>
+           <p>Purchase Order</p>
+         </div>
+         <div className='sale_dropdown_tab'>
+            <p>Purchase Return</p>
+         </div>
       </div>
       <div className='side_function'>
          <SellIcon />
@@ -70,11 +93,11 @@ function Sidebar() {
          <p style={{marginLeft : '1vw'}}>Expenses</p>
       </div>
       <div className='side_function'>
-         <AttachMoneyIcon />
+         <AccountBalanceIcon />
          <p style={{marginLeft : '1vw'}}>Cash & Bank</p>
       </div>
       <div className='side_function'>
-         <AttachMoneyIcon />
+         <AssessmentIcon />
          <p style={{marginLeft : '1vw'}}>Reports</p>
       </div>
     </div>
